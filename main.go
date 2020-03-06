c450dcf492a7935e29a41a57380c04a234550812
nlarsen7 <nlarsen2013@gmail.com>
2020-03-06 02:18:27 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+//Nicholas Larsen
+//March 5, 2020
+//an array that starts at index 1 instead of 0 and fills it with random numbers
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+func main() {
+  rand.Seed(time.Now().UnixNano())
+  var broken [7]int
+  //creates array
+
+  for i:=1; i<7; i++{
+    broken[i]= rand.Intn((20)+1)
+    //loops starting at index
+  }
+  fmt.Println(broken)
+}
